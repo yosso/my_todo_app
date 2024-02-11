50218a62e538db27f9fbd2e6d97b8d98fd50e614
Michael Jones <mike.ecinwa@gmail.com>
2024-02-11 14:29:03 -0600

    Created the cli and gui with some interactions - Day 16

---
+++ app1/gui.py
@@ -0,0 +1,14 @@
+import functions
+import PySimpleGUI as sg
+
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="Enter todo")
+add_buttton = sg.Button("Add")
+
+window = sg.Window('My To-Do App', layout=[[label], [input_box, add_buttton]])
+window.read()
+print('Hello')
+window.close()
+
+
+
