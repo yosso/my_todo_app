9c2240903bfdd1c6c120497641c1305ffcb0113b
Michael Jones <mike.ecinwa@gmail.com>
2024-02-15 21:17:18 -0600

    Added a form/gui with an add button

--- app1/gui.py
+++ app1/gui.py
@@ -2,12 +2,28 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
-add_buttton = sg.Button("Add")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
+add_buttton = sg.Button("Add", key="Add")
 
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_buttton]])
-window.read()
-print('Hello')
+window = sg.Window('My To-Do App',
+                   layout=[[label], [input_box, add_buttton]],
+                   font=('Helvetica', 20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
+
+
 window.close()
 
 
